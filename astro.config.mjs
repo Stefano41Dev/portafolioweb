581fe97001a74b05d24c0f081247b8a8ca30e447
Stefano <stefano.41.dev@gmail.com>
2025-10-07 20:55:46 -0500

    configuracion deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://stefano41dev.github.io/',
+    base: 'portafolioweb'
+});
