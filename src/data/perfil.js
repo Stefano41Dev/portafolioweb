133237b7d793a44c16fd93a2521be85bc585659b
Stefano <stefano.41.dev@gmail.com>
2025-10-08 13:28:48 -0500

    modificando el acceso a rutas para produccion y dev

--- src/data/perfil.js
+++ src/data/perfil.js
@@ -1,10 +1,13 @@
+const BASE = import.meta.env.BASE_URL || '/';
+
+
 export const DATOS_PERFIL = {
     nombresCompleto: "Stefano Alexandro Gonzales Reyna",
     linksSociales: {
         github: "https://github.com/Stefano41Dev",
         linkdin: "https://www.linkedin.com/in/stefano-gonzales-reyna-17ba6b362/",
         gmail: "mailto:stefano41dev@gmail.com",
-        cv: "portafolioweb/Stefano_Gonzales_CV.pdf"
+        cv: BASE + "Stefano_Gonzales_CV.pdf"
     },
     sobreMi: " Estudiante	de	la	carrera	de	Computación	e	Informática	con	interés	en	el	desarrollo	backend.	Me	caracterizo	por	ser	meticuloso	con	mi trabajo	y	aportar	soluciones	de	calidad.	Me	gusta	apoyar	y	compartir	mis	conocimientos	con	el	resto	de	compañeros.	Busco oportunidades	para aplicar mis habilidades en	entornos desafiantes y seguir creciendo	profesionalmente como programador",
 }
